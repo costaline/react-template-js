30e66fe2f879952d9bc56863cc0eaa88aa03182b
Constantine Zubarev <kv.zubarev@gmail.com>
2021-09-17 20:36:08 +0600

    lint: update rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -65,6 +65,7 @@ module.exports = {
       { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*'},
       { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var']},
     ],
+    'spaced-comment': 'warn',
 
     'import/export': 'off',
     'import/no-unresolved': 'error',
--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   "extends": [
     "stylelint-config-recommended",
     "stylelint-config-recess-order",
-    "stylelint-prettier/recommended",
   ],
 
   "plugins": [
@@ -16,15 +15,18 @@ module.exports = {
       "except": [ "blockless-after-same-name-blockless", "first-nested" ],
       "ignoreAtRules": [ "else", "include" ]
     }],
+    "at-rule-no-unknown": null,
     "declaration-colon-space-after": "always-single-line",
     "declaration-colon-space-before": "never",
     "indentation": 2,
     "rule-empty-line-before": ["always", {
       "except": ["after-single-line-comment", "first-nested"]
     }],
+
     "scss/dollar-variable-first-in-block": [true, {
       "ignore": ["comments", "imports"]
     }],
+    "scss/at-rule-no-unknown": true,
     "scss/selector-no-redundant-nesting-selector": true,
   }
 }
